7487ba4a591b45d145cc60f53097dd2a84328b5f
MaryMaina98 <marieshix98@gmail.com>
2021-08-04 23:38:57 +0300

    local storage

--- static/index.js
+++ static/index.js
@@ -1,12 +1,11 @@
 //Navbar section
 
-window.addEventListener('scroll', ()=>{
-  const navbar = document.querySelector('.main-navbar')
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".main-navbar");
 
-  navbar.classList.toggle("sticky", window.scrollY > 0)
-  console.log('scroll')
-})
-
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+  console.log("scroll");
+});
 
 const openNavbar = () => {
   const menu = document.querySelector(".menu");
@@ -35,7 +34,6 @@ openNavbar();
 
 const form = document.getElementById("form");
 
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const input = document.getElementById("user-input").value;
@@ -44,196 +42,172 @@ form.addEventListener("submit", (e) => {
     const heading = document.getElementById("results");
     heading.innerHTML = "Please try again ";
     document.getElementById("main-div").innerHTML = "";
-  }else{
+  } else {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}`)
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data)
-      
-      // const arr= data.items.map(item=>{
-      //   const categories = item.volumeInfo.categories;
-      //   // console.log(categories.toString());
-      //   // const randomSearch = Math.floor(Math.random() * Math.floor(6));
-      //   // console.log(randomSearch);
+      .then((response) => response.json())
+      .then((data) => {
+        displayData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
 
-      // })
-      displayData(data)
-      
-      
-    })
-    .catch((err) => {
-      // console.log(err);
+// const displayCategory = (data)=>{
+//   console.log('category');
+// }
+
+const displayData = (data) => {
+  const input = document.getElementById("user-input").value;
+
+  document.getElementById("results").innerHTML = `Search results for ${input}`;
+
+  const newDiv = document.createElement("div");
+
+  const bookCollection= []
+
+  data.items.forEach((elem, index) => {
+    const html = `
+  
+    
+      <img src="${elem.volumeInfo.imageLinks.smallThumbnail}"/>
+      <p>Title: ${elem.volumeInfo.title}</p>
+      <p>Author: ${elem.volumeInfo.authors}</p>
+   
+      <p> Info: ${elem.searchInfo.textSnippet}</p>
+     
+      <a href="${elem.volumeInfo.infoLink}" target="blank">  link: Read More</a>
+
+   
+  `;
+    const div = document.createElement("div");
+    div.setAttribute("id", `${index}`);
+    div.setAttribute("class", "bookItem");
+
+    const content = document.createElement("div");
+    content.setAttribute('class', 'book-wrapper')
+    content.innerHTML = html;
+    const button = document.createElement("button");
+    button.setAttribute("class", "btn-add");
+
+    button.innerText = "Add to collection";
+    content.appendChild(button);
+    div.appendChild(content);
+    
+
+    button.addEventListener("click", (e) => {
+      console.log(elem);
+      bookCollection.push(elem)
+      localStorage.setItem('bk', JSON.stringify(bookCollection))
     });
 
-  }
-   
+    newDiv.appendChild(div);console.log(bookCollection);
+  });
+  
+  const btn = document.createElement("button");
+  const txt = document.createTextNode("Add to collection");
+  newDiv.appendChild(btn.appendChild(txt));
+  document.getElementById("main-div").appendChild(newDiv);
+
+  const addToCart = document.querySelectorAll(".btn-add");
+
   
 
-  })
-
-  // const displayCategory = (data)=>{
-  //   console.log('category');
-  // }
-
-  const displayData = (data) => {
-    const input = document.getElementById('user-input').value
-  
-   document.getElementById("results").innerHTML= `Search results for ${input}`
-  
-   
-  const newDiv= document.createElement('div')
-  
-  newDiv.innerHTML =data.items.map(
-    (elem, index) =>
-    //   const strlength= elem.searchInfo.textSnippet.strlength;
-    //   if(!strlength > 10){
-    //  return elem.searchInfo.textSnippet
-    //   }else{
-    //     const newStr = elem.searchInfo.textSnippet
-    //   }
-  
-  
-      `
-    
-      <div class ='book-wrapper'>
-        <img src="${elem.volumeInfo.imageLinks.smallThumbnail}"/>
-        <p>Title: ${elem.volumeInfo.title}</p>
-        <p>Author: ${elem.volumeInfo.authors}</p>
-     
-        <p> Info: ${elem.searchInfo.textSnippet}</p>
-       
-        <a href="${elem.volumeInfo.infoLink}" target="blank">  link: Read More</a>
-        <button class="btn-add">Add to collection</button>
-    
-    
-     </div>
-    `
-    
-   
-     
-  );
-  const btn = document.createElement('button')
-  const txt= document.createTextNode('Add to collection')
-  newDiv.appendChild(btn.appendChild(txt))
-  document.getElementById('main-div').appendChild(newDiv);
-  
-   const addToCart= document.querySelectorAll('.btn-add');
-   let adjusted = []
 
 
-   const arrItems= data.items.map((el, index)=>{
-  
-    const obj ={
-      img:  el.volumeInfo.imageLinks.smallThumbnail,
-      title:el.volumeInfo.title,
-       authors: el.volumeInfo.authors,
-       textt: el.searchInfo.textSnippet,
-       link: el.volumeInfo.infoLink
-    }
-adjusted.push(obj)
-   })
+
+  let adjusted = [];
+
+  const arrItems = data.items.map((el, index) => {
+    const obj = {
+      img: el.volumeInfo.imageLinks.smallThumbnail,
+      title: el.volumeInfo.title,
+      authors: el.volumeInfo.authors,
+      textt: el.searchInfo.textSnippet,
+      link: el.volumeInfo.infoLink,
+    };
+    adjusted.push(obj);
+  });
   //  console.log(adjusted)
 
-   for(i =0; i< addToCart.length; i++){
-     
-    console.log(adjusted[i])
-      
-     addToCart[i].addEventListener('click', ()=>{
-      
-       addBooks(adjusted[i]);
-     })
-   }
-    document.getElementById('user-input').value=" "
-    document.getElementById("results").innerHTML = " "
-   
+  for (i = 0; i < addToCart.length; i++) {
+    addToCart[i].addEventListener("click", () => {
+      console.log(adjusted[i]);
+      //addBooks(adjusted[i]);
+    });
   }
- 
-  
- // const arr = data.items.map((elem, index) => {
-    // document.getElementById("results").innerHTML = " "
-    // console.log(elem.volumeInfo.title);
+  document.getElementById("user-input").value = " ";
+  document.getElementById("results").innerHTML = " ";
+};
 
-    // const authors = elem.volumeInfo.authors;
-    // authors.map((author, index) => {
-    //   console.log(author);
-    // });
+// const arr = data.items.map((elem, index) => {
+// document.getElementById("results").innerHTML = " "
+// console.log(elem.volumeInfo.title);
 
-    // const wrapper = document.createElement("div");
-    // wrapper.classList.add("details");
-    // const title = document.createTextNode(
-    //   `Book title: ${elem.volumeInfo.title}`
-    // );
-    // const titleContainer = document.createElement("div");
-    // titleContainer.appendChild(title);
-    // const image= document.createElement('img')
+// const authors = elem.volumeInfo.authors;
+// authors.map((author, index) => {
+//   console.log(author);
+// });
 
-    //  const img= image.setAttribute("src", `${elem.volumeInfo.imageLinks.smallThumbnail}`);
+// const wrapper = document.createElement("div");
+// wrapper.classList.add("details");
+// const title = document.createTextNode(
+//   `Book title: ${elem.volumeInfo.title}`
+// );
+// const titleContainer = document.createElement("div");
+// titleContainer.appendChild(title);
+// const image= document.createElement('img')
 
-    //mainDiv
+//  const img= image.setAttribute("src", `${elem.volumeInfo.imageLinks.smallThumbnail}`);
+
+//mainDiv
 //  const img= image.setAttribute("src", `${elem.volumeInfo.imageLinks.smallThumbnail}`);
 // wrapper.appendChild(img);
-    // wrapper.appendChild(titleContainer);
-    //document.getElementById('main-div').appendChild(wrapper)
- // });
+// wrapper.appendChild(titleContainer);
+//document.getElementById('main-div').appendChild(wrapper)
+// });
 
+const checkCollection = () => {
+  let collection = localStorage.getItem("collectionNumber");
+  if (collection) {
+    document.querySelector(".count").textContent = collection;
+    console.log("colection");
+  }
+};
 
-const checkCollection =()=>{
-let collection= localStorage.getItem('collectionNumber')
-if(collection){
-  document.querySelector('.count').textContent = collection
-  console.log('colection')
-}
-
-}
-  
-
-const addBooks =(arrItems)=>{
+const addBooks = (arrItems) => {
   //console.log('Add to collection')
-  const {img, textt, link, authors, textt}= arrItems
- console.log(`The product clicked is ${img} `)
+  const { img, link, authors, textt } = arrItems;
+  console.log(`The product clicked is ${img} `);
 
   //show the book collection wrapper
-  const bookWrapper= document.querySelector('.cart');
-  bookWrapper.classList.add('show-cart')
+  const bookWrapper = document.querySelector(".cart");
+  bookWrapper.classList.add("show-cart");
 
-  
+  let collection = parseInt(localStorage.getItem("collectionNumber"));
+  // console.log(typeof collection)
 
-  let collection= parseInt(localStorage.getItem('collectionNumber'))
- // console.log(typeof collection)
-
-//check if there is any item in the localstorage
-  if(!collection){
-    localStorage.setItem('collectionNumber', 1)
-    let bookCount=document.querySelector('.count').textContent = collectionNumber
-  }else{
-    localStorage.setItem('collectionNumber', collection + 1)
-    document.querySelector('.count').textContent= collection + 1;
-    
-  }        
-}
-  
-
- 
-  
-
-
-  
-    
-  
-
- 
-
+  //check if there is any item in the localstorage
+  if (!collection) {
+    localStorage.setItem("collectionNumber", 1);
+    let bookCount = (document.querySelector(".count").textContent =
+      collectionNumber);
+  } else {
+    localStorage.setItem("collectionNumber", collection + 1);
+    document.querySelector(".count").textContent = collection + 1;
+  }
+};
 
 checkCollection();
 const text = document.querySelector(".text");
 const btnRead = document.querySelector(".btn");
 
 btnRead.addEventListener("click", (e) => {
-text.classList.toggle("active");
+  text.classList.toggle("active");
   if (btnRead.innerHTML == "Read More") {
     btnRead.innerHTML = "Read Less";
   } else {
     btnRead.innerHTML = "Read More";
   }
 });
-
